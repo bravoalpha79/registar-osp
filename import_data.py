870cc39b946e4cd27cafd2aa44668de4c19055b9
bravoalpha79 <igor.basuga@gmail.com>
2023-09-10 22:58:19 +0200

    Code/linter cleanup

--- import_data.py
+++ import_data.py
@@ -1,3 +1,5 @@
+# script used to import initial (test) data into Postgres (via Shell)
+
 import json
 from django.db import IntegrityError
 from django.contrib.gis.geos import GEOSGeometry
@@ -10,16 +12,16 @@ for ft in data['features']:
     geom_str = json.dumps(ft['geometry'])
     geom = GEOSGeometry(geom_str)
     safety_object = SafetyObject(
-        naziv_objekta = properties['naziv_objekta'],
-        ps_br = properties['ps_br'],
-        e_br = properties['e_br'],
-        tip_objekta = properties['tip_objekta'],
-        lucka_kapetanija = properties['lucka_kapetanija'],
-        fotografija = properties['fotografija'],
-        id_ais = properties['id_ais'],
-        simbol_oznaka = properties['simbol_oznaka'],
-        pk = int(properties['pk']),
-        lokacija = geom
+        naziv_objekta=properties['naziv_objekta'],
+        ps_br=properties['ps_br'],
+        e_br=properties['e_br'],
+        tip_objekta=properties['tip_objekta'],
+        lucka_kapetanija=properties['lucka_kapetanija'],
+        fotografija=properties['fotografija'],
+        id_ais=properties['id_ais'],
+        simbol_oznaka=properties['simbol_oznaka'],
+        pk=int(properties['pk']),
+        lokacija=geom
     )
     try:
         safety_object.save()
